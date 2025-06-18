7b38c3929ead8552b7c930fe0bd4f2c9860c87c8
Jesús González <jrgonzalez@wyndhamconcorde.com>
2025-06-18 16:49:17 -0400

    chore: update

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require("calypso.core")
+require("calypso.lazy")
--- lua/calypso/core/keymaps.lua
+++ lua/calypso/core/keymaps.lua
@@ -14,3 +14,9 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equally sized" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split"})
+
+keymap.set("n", "<leader>to","<cmd>tabnew<CR>", { desc = "Open new tab" }) -- Open new tab
+keymap.set("n", "<leader>tx","<cmd>tabclose<CR>", { desc = "Close new tab" }) -- Close new tab
+keymap.set("n", "<leader>tn","<cmd>tabn<CR>", { desc = "Go to next tab" }) -- Go to next tab 
+keymap.set("n", "<leader>tp","<cmd>tabp<CR>", { desc = "Go to previous tab" }) -- Go to previous tab
+keymap.set("n", "<leader>tf","<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
