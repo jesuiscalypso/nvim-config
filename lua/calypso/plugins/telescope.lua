f8ceac1591c3c4c51dd02dd80209338d4a62a2d6
Jesús González <jrgonzalez@wyndhamconcorde.com>
2025-07-01 08:59:38 -0400

    added alpha greeter

--- lua/calypso/plugins/telescope.lua
+++ lua/calypso/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
         
         telescope.setup({
             defaults = {
-                path_display = "smart",
+                path_display = {"smart"},
                 mappings = {
                     i = {
                         ["<C-k>"] = actions.move_selection_previous, -- 
