a41962d89f505c0b5a0264c37907cb739bbab8e5
Jesús González <jrgonzalez@wyndhamconcorde.com>
2025-07-02 15:20:01 -0400

    added lualine and bufferline

--- lua/calypso/lazy.lua
+++ lua/calypso/lazy.lua
@@ -11,4 +11,12 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("calypso.plugins")
+require("lazy").setup("calypso.plugins", {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false
+    },
+})
---
+++ lua/calypso/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+    -- Makes tabs look nicer, apparently
+    "akinsho/bufferline.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    version  = "*",
+    opts = {
+        options = {
+            mode = "tabs",
+            separator_style = "slant",
+        },
+    },
+}
