659892dd5d37aa808a32e845fabac2b6065d276c
Calypso <jesuiscalypso@protonmail.com>
2025-07-26 09:23:59 -0400

    began setting up lsp

--- lua/calypso/lazy.lua
+++ lua/calypso/lazy.lua
@@ -11,7 +11,7 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("calypso.plugins", {
+require("lazy").setup({{ import = "calypso.plugins"} , {import = "calypso.plugins.lsp"}}, {
     checker = {
         enabled = true,
         notify = false,
