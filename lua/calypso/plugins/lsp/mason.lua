c4236b8f922192565b5e7a9449184aad5b1183dc
Jesús González <jrgonzalez@wyndhamconcorde.com>
2025-09-09 12:33:00 -0400

    linting

--- lua/calypso/plugins/lsp/mason.lua
+++ lua/calypso/plugins/lsp/mason.lua
@@ -1,19 +1,32 @@
 return {
-  "mason-org/mason.nvim",
-  config = function()
-    -- import mason
-    local mason = require("mason")
+    "mason-org/mason.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+        -- import mason
+        local mason = require("mason")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+        local mason_tool_installer = require("mason-tool-installer")
+        -- enable mason and configure icons
 
-  end,
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua",   -- lua formatter
+                "isort",    -- python formatter
+                "black",    -- python formatter
+                "pylint",   -- python linter
+                "eslint_d", -- js linter
+            },
+        })
+    end,
 }
--- lua/calypso/plugins/telescope.lua
+++ lua/calypso/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        {"nvim-telescope/telescope-fzf-native.nvim", build = "make"},
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         "nvim-tree/nvim-web-devicons",
         "folke/todo-comments.nvim"
     },
@@ -13,12 +13,12 @@ return {
 
         telescope.setup({
             defaults = {
-                path_display = {"smart"},
+                path_display = { "smart" },
                 mappings = {
                     i = {
-                        ["<C-k>"] = actions.move_selection_previous, -- 
-                        ["<C-j>"] = actions.move_selection_next, -- 
-                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- 
+                        ["<C-k>"] = actions.move_selection_previous,                       --
+                        ["<C-j>"] = actions.move_selection_next,                           --
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, --
                     },
                 }
             },
@@ -30,10 +30,10 @@ return {
 
         local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {desc = "Fuzzy find files in cwd"})
-        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", {desc = "Fuzzy find recent files"})
-        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", {desc = "Fuzzy find string in cwd"})
-        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", {desc = "FInd string under crsor in cwd"})
-        keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", {desc = "Find TODOs"})
+        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Fuzzy find string in cwd" })
+        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "FInd string under crsor in cwd" })
+        keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find TODOs" })
     end,
 }
